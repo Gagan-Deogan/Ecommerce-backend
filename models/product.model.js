136ce2f5d4f7c2305b6203bb24542158c600c92f
GagandeepSingh0115 <gs816912@gmail.com>
2021-05-06 23:16:38 +0530

    fixes:product-model

--- models/product.model.js
+++ models/product.model.js
@@ -1,65 +1,57 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
-const ProductSchema = new Schema({ 
+const Schema = mongoose.Schema;
+const ProductSchema = new Schema({
   name: {
-    type:String,
-    required:"name is required",
-    unique: "name must be unique"
+    type: String,
+    required: "name is required",
+    unique: "name must be unique",
   },
   price: {
-    type:Number,
-    required:"price is required"
+    type: Number,
+    required: "price is required",
   },
-  categoryId:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Catagory",
-    required:"model name is required"
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Catagory",
+    required: "model name is required",
   },
-  discount:{
-    type:Number,
+  discount: {
+    type: Number,
     min: [0, "discount policy should not allow less than 0%"],
     max: [60, "discount policy should not allow more than 60%"],
-    required:"price is required"
+    required: "price is required",
   },
   effectivePrice: {
-    type:Number,
-    required:"effectivePrice is required"
+    type: Number,
+    required: "effectivePrice is required",
   },
-  rating:{
-    type:Number,
+  rating: {
+    type: Number,
     min: [1, "Rating should not allow less than 1"],
     max: [5, "Rating should not allow less than 5"],
-    required:"Rating is required"
+    required: "Rating is required",
   },
-  rating:{
-    type:Number,
-    min: [1, "Rating should not allow less than 1"],
-    max: [5, "Rating should not allow less than 5"],
-    required:"Rating is required"
+  avalibility: {
+    type: Boolean,
+    required: "Avalibility is required",
   },
-  avalibility:{
-    type:Boolean,
-    required:"Avalibility is required"
+  fastDelivery: {
+    type: Boolean,
+    required: "Avalibility is required",
   },
-  fastDelivery:{
-    type:Boolean,
-    required:"Avalibility is required"
+  label: {
+    type: String,
   },
-  label:{
-    type:String,
+  description: {
+    type: String,
+    required: "Description is required",
   },
-  description:{
-    type:String,
-    required:"Description is required"
+  image: {
+    type: String,
+    required: "Image is required",
   },
-  image:{
-    type:String,
-    required:"Image is required"
-  }
 });
-
 
 const Product = mongoose.model("Product", ProductSchema);
 
-
-module.exports = { Product }
+module.exports = { Product };
