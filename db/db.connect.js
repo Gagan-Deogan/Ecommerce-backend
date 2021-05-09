a92457665134b6e4c5b5b67465af69e7164543ca
GagandeepSingh0115 <gs816912@gmail.com>
2021-05-09 15:27:48 +0530

    fixes:review fixes and modifiaction

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,18 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const yenv = require("yenv");
+const env = yenv("app.yaml", { env: "env_variables" });
 
+const mySecret = env["DBPASSWORD"];
 
-const mySecret = process.env['monogoDB_password']
-
-function initializeDBConnection() {
-  // Connecting to DB
-  mongoose.connect(`mongodb+srv://Gagandeep:${mySecret}@videolibaray.licm8.mongodb.net/inventory?retryWrites=true&w=majority`, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-  })
-    .then(() => console.log("successfully connected"))
-    .catch(error => console.error("mongoose connection failed...", error))
-}
-
-module.exports = { initializeDBConnection }
-
+const initializeDBConnection = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://Gagandeep:${mySecret}@videolibaray.licm8.mongodb.net/inventory?retryWrites=true&w=majority`,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
+  } catch (error) {
+    return error;
+  }
+};
+module.exports = { initializeDBConnection };
